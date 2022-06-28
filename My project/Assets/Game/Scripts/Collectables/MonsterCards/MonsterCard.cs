ed64ad12237f4fb70baa28514f43058627636feb
Bacoq4 <53489761+Bacoq4@users.noreply.github.com>
2022-06-28 12:58:57 +0300

    initialize classes

---
+++ My project/Assets/Game/Scripts/Collectables/MonsterCards/MonsterCard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoreGame.Collectable
+{
+    public class MonsterCard : Collectable
+    {
+        
+    }
+
+}
